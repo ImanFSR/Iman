9e8b780b6770c3764ee52458f758c98638ebe299
ImanFSR <falahsulaimanranardy@gmail.com>
2025-07-23 22:26:35 +0700

    Add files via upload

---
+++ datadiri.py
@@ -0,0 +1,28 @@
+#Percobaan data diri
+
+print("=====================================")
+
+print("Selamat datang di Program Data Diri Sederhana dengan menggunakan Python")
+print("Mohon maaf jika ada kesalahan dalam program ini ya!")
+
+print("=====================================")
+
+name = input("Masukkan nama lengkap anda: ")
+nama = input("Masukkan nama panggilan anda: ")
+age = int(input("Masukkan umur anda: "))
+sch = input("Masukkan nama tempat sekolah/pekerjaan anda: ")
+print("=====================================")
+
+print("Terima kasih sudah mengisi data diri anda!")
+print("Berikut adalah data diri anda:")
+print(f"Nama Lengkap: {name}")
+print(f"Nama Panggilan: {nama}")
+print(f"Umur sekarang: {age} tahun")
+print(f"Tempat Sekolah/Pekerjaan anda: {sch}")
+
+print("======================================")
+
+print("Terima kasih telah menggunakan program ini!")
+print("Sampai jumpa di lain waktu ya!")
+
+print("======================================")
