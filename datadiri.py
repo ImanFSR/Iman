1e21b6593a3d319a436d7ac55145f7b0def3a5e7
ImanFSR <falahsulaimanranardy@gmail.com>
2025-08-08 18:51:43 +0700

    Update datadiri.py

--- datadiri.py
+++ datadiri.py
@@ -1,12 +1,12 @@
 # datadiri.py
 garis = "=" * 40
 
-print("garis")
+print(garis)
 
 print("Selamat datang di Program Data Diri Sederhana dengan menggunakan Python")
 print("Mohon maaf jika ada kesalahan dalam program ini ya!")
 
-print("garis")
+print(garis)
 
 name = input("Masukkan nama lengkap anda: ")
 nama = input("Masukkan nama panggilan anda: ")
@@ -15,7 +15,7 @@ sch = input("Status sekarang? (Mahasiswa/Kerja/Nganggur): ").lower()
 if sch == "mahasiswa":
     kampus = input("Masukkan nama tempat kampus anda: ")
     smt = int(input("Masukkan semester anda sekarang: "))
-    print("garis")
+    print(garis)
     print(f"Nama panjang anda adalah {name}, dan nama panggilan anda adalah {nama}.")
     print(f"Umur anda sekarang adalah {age} tahun.")
     print(f"Anda adalah seorang {sch} semester {smt} dari kampus {kampus}.")
@@ -23,17 +23,17 @@ if sch == "mahasiswa":
 if sch == "kerja":
     kerja = input("Masukkan nama tempat kerja anda: ")
     jbt = input("Masukkan jabatan anda: ")
-    print("garis")
+    print(garis)
     print(f"Nama panjang anda adalah {name}, dan nama panggilan anda adalah {nama}.")
     print(f"Umur anda sekarang adalah {age} tahun, dan anda sekarang {sch} di {kerja} sebagai {jbt}.")
 
 if sch == "nganggur":
-    print("garis")
+    print(garis)
     print(f"Nama panjang anda adalah {name}, dan nama panggilan anda adalah {nama}.")
     print(f"Umur anda sekarang adalah {age} tahun, dan anda sekarang adalah seorang pengagguran ga guna.")
 
 elif sch not in ["mahasiswa", "kerja", "nganggur"]:
-    print("garis")
+    print(garis)
     print("Status tidak valid, silakan masukkan Mahasiswa, Kerja, atau Nganggur.")
-print("garis")
+print(garis)
 print("Terima kasih sudah mengisi data diri anda!")
